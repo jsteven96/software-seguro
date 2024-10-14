c778c633e5999c589cead59869bf5547d30b94dc
javierPerezSB <javierstevenperezfernandez@MacBook-Pro-de-Javier.local>
2024-10-14 13:47:31 -0500

    Primer commit

---
+++ src/main/java/com/learn/webhooks/github/WebhooksController.java
@@ -0,0 +1,25 @@
+package com.learn.webhooks.github;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class WebhooksController {
+
+    private static final Logger log = LoggerFactory.getLogger(WebhooksController.class);
+
+
+    @PostMapping(path = "hello")
+    public Map<String, Object> getWebHook(
+            @RequestBody Map<String, Object> body
+            ) {
+        log.info("Receiving a request");
+        log.info(body.toString());
+        return body;
+    }
+}
